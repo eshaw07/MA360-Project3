50a716c25391c1d59c0249e9cb609db1d0ea08d5
eshaw07 <99222686+eshaw07@users.noreply.github.com>
2022-04-11 23:37:33 -0500

    Update ForwardSub.py

--- ForwardSub.py
+++ ForwardSub.py
@@ -1,3 +1,4 @@
+# Question 1
 def forwardSub(L, b):
     x = []
     for i in range(len(b)):
@@ -25,5 +26,12 @@ L = [[7,1],[0, 3.9]]
 b = [2, 2]
 
 print("Test 3: ")
+x = forwardSub(L, b)
+print("The Solution is: ", x)
+
+# Question 4
+L = [[4, 2, 6, 2, 4, 1, 7, 8, 9, 4], [5, 3, 8, 2, 3, 5, 2, 1, 8, 5]]
+b = [1, 1]
+print("Test 4: ")
 x = forwardSub(L, b)
 print("The Solution is: ", x)
